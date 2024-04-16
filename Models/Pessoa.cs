769b7eb4193fa445861cc9d10c7f774eb1797694
[200~CamilaFerreiraRodrigues~ <ca.rodrigues02@outlook.com>
2024-04-15 23:20:01 -0300

    Metodo sem exceção

---
+++ Models/Pessoa.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_.Models
+{
+    public class Pessoa
+    {
+
+        //propriedades (digira prop) -> se há get e set = propriedades
+        public String Nome { get; set; }
+        public int Idade { get; set; }
+
+        // criando metodos (ações)
+
+        public void Apresentar(){
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using C_.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Davi";
+p1.Idade = 15;
+
+
+p1.Apresentar();
