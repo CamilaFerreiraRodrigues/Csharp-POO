750ca75e727d509a070d11b01cd5e362de3b79f1
[200~CamilaFerreiraRodrigues~ <ca.rodrigues02@outlook.com>
2024-04-17 21:58:57 -0300

    validando atributos

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -9,13 +9,64 @@ namespace C_.Models
     {
 
         //propriedades (digita prop) -> se há get e set = propriedades
-        public String Nome { get; set; }
-        public int Idade { get; set; }
+
+        // Se colocamos o get e set vazio -> aceita qualquer valor
+
+
+        // deixando Nome obrigatorio - protegendo a propriedade Nome
+        private string _nome;
+        private int _idade;
+        public String Nome 
+        { 
+            /*get{
+                return _nome.ToUpper();
+            }*/
+
+            get => _nome.ToUpper();
+            
+            set {
+
+                if (value == ""){
+                    //exceção - validando se o nome está vazio
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            } 
+            
+            
+        }
+
+        public string Sobrenome{ get; set; }
+
+        public  string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+       //public int Idade { get; set; }
+
+       //Validando a Idade
+
+       
+       public int Idade { 
+        
+        get => _idade;
+        
+        
+        set{
+            if (value < 0){
+                throw new ArgumentException("A idade não pode ser menor do que zero");
+
+            }
+
+            _idade = value;
+        }
+        
+        
+    }
+
 
         // criando metodos (ações)
 
         public void Apresentar(){
-            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
         }
 
 
--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using C_.Models;
 
 Pessoa p1 = new Pessoa();
-p1.Nome = "Davi";
-p1.Idade = 15;
-
+p1.Nome = "Camila";
+p1.Idade = -1;
+p1.Sobrenome = "Fereira";
 
 p1.Apresentar();
